18922aaf0188744dca11147dd20ae6b4ea60b8cc
Arona <aronaboye70@gmail.com>
2023-09-26 23:48:59 +0000

    responsive bar et changement hamburger en X

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const nav = document.querySelector("header div.nav")
 burger.addEventListener("click",()=>{
     nav.classList.toggle("navToogle");
 })
+burger.addEventListener("click",() => {
+    burger.classList.toggle("active")
+})
